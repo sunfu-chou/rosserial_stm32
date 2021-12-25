665724c20dc5379a1afea3d330d2081204ecf33c
sunfu-chou <sunfu.chou@gmail.com>
2021-12-25 22:25:36 +0800

    update make_libraries and delete example, docker support for new STM32CUBEIDE 1.8

--- src/rosserial_stm32/make_libraries.py
+++ src/rosserial_stm32/make_libraries.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #####################################################################
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 2018, Kenta Yonekura (a.k.a. yoneken), 
+# Copyright (c) 2018, Kenta Yonekura (a.k.a. yoneken),
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -39,7 +39,7 @@ __usage__ = """
 make_libraries.py generates the STM32 rosserial library files for SW4STM32.
 It requires the location of your SWSTM32 project folder.
 
-rosrun rosserial_stm32 make_libraries.py <output_path>
+rosrun rosserial_stm32 make_libraries.py <stm32_proj_path>
 """
 
 import rospkg
@@ -50,46 +50,63 @@ from rosserial_client.make_library import *
 import shutil
 
 ROS_TO_EMBEDDED_TYPES = {
-    'bool'    :   ('bool',              1, PrimitiveDataType, []),
-    'byte'    :   ('int8_t',            1, PrimitiveDataType, []),
-    'int8'    :   ('int8_t',            1, PrimitiveDataType, []),
-    'char'    :   ('uint8_t',           1, PrimitiveDataType, []),
-    'uint8'   :   ('uint8_t',           1, PrimitiveDataType, []),
-    'int16'   :   ('int16_t',           2, PrimitiveDataType, []),
-    'uint16'  :   ('uint16_t',          2, PrimitiveDataType, []),
-    'int32'   :   ('int32_t',           4, PrimitiveDataType, []),
-    'uint32'  :   ('uint32_t',          4, PrimitiveDataType, []),
-    'int64'   :   ('int64_t',           8, PrimitiveDataType, []),
-    'uint64'  :   ('uint64_t',          8, PrimitiveDataType, []),
-    'float32' :   ('float',             4, PrimitiveDataType, []),
-    'float64' :   ('double',            8, PrimitiveDataType, []),
-    'time'    :   ('ros::Time',         8, TimeDataType, ['ros/time']),
-    'duration':   ('ros::Duration',     8, TimeDataType, ['ros/duration']),
-    'string'  :   ('char*',             0, StringDataType, []),
-    'Header'  :   ('std_msgs::Header',  0, MessageDataType, ['std_msgs/Header'])
+    "bool": ("bool", 1, PrimitiveDataType, []),
+    "byte": ("int8_t", 1, PrimitiveDataType, []),
+    "int8": ("int8_t", 1, PrimitiveDataType, []),
+    "char": ("uint8_t", 1, PrimitiveDataType, []),
+    "uint8": ("uint8_t", 1, PrimitiveDataType, []),
+    "int16": ("int16_t", 2, PrimitiveDataType, []),
+    "uint16": ("uint16_t", 2, PrimitiveDataType, []),
+    "int32": ("int32_t", 4, PrimitiveDataType, []),
+    "uint32": ("uint32_t", 4, PrimitiveDataType, []),
+    "int64": ("int64_t", 8, PrimitiveDataType, []),
+    "uint64": ("uint64_t", 8, PrimitiveDataType, []),
+    "float32": ("float", 4, PrimitiveDataType, []),
+    "float64": ("double", 8, PrimitiveDataType, []),
+    "time": ("ros::Time", 8, TimeDataType, ["ros/time"]),
+    "duration": ("ros::Duration", 8, TimeDataType, ["ros/duration"]),
+    "string": ("char*", 0, StringDataType, []),
+    "Header": ("std_msgs::Header", 0, MessageDataType, ["std_msgs/Header"]),
 }
 
 # need correct inputs
-if (len(sys.argv) < 2):
-    print __usage__
+if len(sys.argv) < 2:
+    print(__usage__)
     exit()
-    
+
+# check if path is correct
+if not os.path.exists(os.path.join(sys.argv[1], "Core", "Inc")):
+    print("\033[91m", "Path not existing: %s" % os.path.join(sys.argv[1], "Core", "Inc"), "\033[0m")
+    exit()
+
 # get output path
-path = sys.argv[1]
-if path[-1] == "/":
-    path = path[0:-1]
-print "\nExporting to %s" % path
+roslib_path = os.path.join("Core", "Inc", "ROSLib")
+output_path = os.path.join(sys.argv[1], roslib_path)
+print("\033[93m", "\nExporting to: %s" % output_path, "\033[0m")
 
 rospack = rospkg.RosPack()
+rosserial_stm32_dir = rospack.get_path(THIS_PACKAGE)
+
+# copy ros library for STM32 hardware in
+if os.path.exists(output_path):
+    # delete duplicated folder
+    print("\033[93m", "\nDelete old folder: %s" % output_path, "\033[0m")
+    shutil.rmtree(output_path, ignore_errors=True)
+shutil.copytree(os.path.join(rosserial_stm32_dir, "src", "ros_lib"), output_path)
 
 # copy ros_lib stuff in
-rosserial_stm32_dir = rospack.get_path(THIS_PACKAGE)
-files = os.listdir(rosserial_stm32_dir+"/src/ros_lib")
-for f in files:
-  if os.path.isfile(rosserial_stm32_dir+"/src/ros_lib/"+f):
-    shutil.copy(rosserial_stm32_dir+"/src/ros_lib/"+f, path+"/Inc/")
-rosserial_client_copy_files(rospack, path+"/Inc/")
+rosserial_client_copy_files(rospack, output_path)
 
 # generate messages
-rosserial_generate(rospack, path+"/Inc/", ROS_TO_EMBEDDED_TYPES)
+rosserial_generate(rospack, output_path, ROS_TO_EMBEDDED_TYPES)
 
+# Move '*.cpp' files in 'Src/' folder:
+cppfiles = [files for files in os.listdir(output_path) if files.endswith(".cpp")]
+stmsrc_path = os.path.normpath(os.path.join(output_path, "..", "..", "Src"))
+if os.path.exists(stmsrc_path):
+    os.makedirs(os.path.join(stmsrc_path, "ROSLib"))
+    for file in cppfiles:
+        shutil.move(os.path.join(output_path, file), os.path.join(stmsrc_path, "ROSLib"))
+else:
+    print("\033[91m", "Path not existing: %s\n Source files are not moved" % stmsrc_path, "\033[0m")
+    exit()
